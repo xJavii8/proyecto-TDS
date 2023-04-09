7b16daa0bfe3b0268bf557112a1cf8cd0275d28b
Javi <jviix8@gmail.com>
2023-04-09 18:59:15 +0200

    Fix addPublication

--- PhotoTDS/src/umu/tds/view/AddPublicationWindow.java
+++ PhotoTDS/src/umu/tds/view/AddPublicationWindow.java
@@ -17,22 +17,6 @@ public class AddPublicationWindow {
 	private JFrame frame;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AddPublicationWindow window = new AddPublicationWindow();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the application.
 	 */
 	public AddPublicationWindow() {
@@ -50,7 +34,7 @@ public class AddPublicationWindow {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
