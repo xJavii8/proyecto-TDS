9e5bcf200fa78a8179c5e2f9f0830e25c260996b
Ignacio <ignacio2002ruiz@gmail.com>
2023-04-09 18:55:51 +0200

    Ventana addPublication

---
+++ PhotoTDS/src/umu/tds/view/AddPublicationWindow.java
@@ -0,0 +1,63 @@
+package umu.tds.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Insets;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public class AddPublicationWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddPublicationWindow window = new AddPublicationWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AddPublicationWindow() {
+		initialize();
+	}
+	
+	public void show() {
+		frame.setVisible(true);
+	}
+	
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
+		
+		JButton btnNewButton = new JButton("New button");
+		panel.add(btnNewButton);
+	}
+
+}
