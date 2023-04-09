5f402e363cf0d5e19914e84ee5641cb292790026
Javi <jviix8@gmail.com>
2023-04-09 18:27:42 +0200

    Fix foto de perfil con espacios + foto de perfil en MainWindow

--- PhotoTDS/src/umu/tds/view/MainWindow.java
+++ PhotoTDS/src/umu/tds/view/MainWindow.java
@@ -4,30 +4,47 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import java.awt.Toolkit;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Cursor;
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainWindow {
 
 	private JFrame frame;
 	private String username;
+	private String profilePicPath;
 
 	/**
 	 * Create the application.
 	 */
-	public MainWindow(String username) {
+	public MainWindow(String username, String profilePicPath) {
 		this.username = username;
+		if (profilePicPath.contains("%")) {
+			try {
+				profilePicPath = URLDecoder.decode(profilePicPath, "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		}
+		this.profilePicPath = profilePicPath;
 		initialize();
 	}
-	
+
 	public void show() {
 		frame.setVisible(true);
 	}
@@ -41,16 +58,16 @@ public class MainWindow {
 		frame.setSize(521, 583);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JPanel panelNorte = new JPanel();
 		frame.getContentPane().add(panelNorte, BorderLayout.NORTH);
 		GridBagLayout gbl_panelNorte = new GridBagLayout();
-		gbl_panelNorte.columnWidths = new int[]{27, 109, 271, 63, 0, 0};
-		gbl_panelNorte.rowHeights = new int[]{12, 32, 0};
-		gbl_panelNorte.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panelNorte.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panelNorte.columnWidths = new int[] { 27, 109, 140, 106, 63, 0, 0 };
+		gbl_panelNorte.rowHeights = new int[] { 12, 32, 0 };
+		gbl_panelNorte.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panelNorte.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
 		panelNorte.setLayout(gbl_panelNorte);
-		
+
 		JLabel logo = new JLabel("PhotoTDS");
 		logo.setIcon(new ImageIcon(MainWindow.class.getResource("/images/ig32.png")));
 		logo.setFont(new Font("Bahnschrift", Font.BOLD, 16));
@@ -59,17 +76,46 @@ public class MainWindow {
 		gbc_logo.gridx = 1;
 		gbc_logo.gridy = 1;
 		panelNorte.add(logo, gbc_logo);
-		
+
 		JLabel profile = new JLabel("username");
+		ImageIcon icon = new ImageIcon(profilePicPath);
+		if (icon.getIconHeight() > 32 || icon.getIconWidth() > 32) {
+			icon = new ImageIcon(icon.getImage().getScaledInstance(32, 32, Image.SCALE_DEFAULT));
+		}
+
+		JLabel uploadPhoto = new JLabel("");
+		uploadPhoto.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Presionado");
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				uploadPhoto.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				uploadPhoto.setCursor(Cursor.getDefaultCursor());
+			}
+		});
+		uploadPhoto.setIcon(new ImageIcon(MainWindow.class.getResource("/images/uploadPhoto.png")));
+		GridBagConstraints gbc_uploadPhoto = new GridBagConstraints();
+		gbc_uploadPhoto.insets = new Insets(0, 0, 0, 5);
+		gbc_uploadPhoto.gridx = 3;
+		gbc_uploadPhoto.gridy = 1;
+		panelNorte.add(uploadPhoto, gbc_uploadPhoto);
+		profile.setIcon(icon);
 		profile.setText(username);
 		profile.setFont(new Font("Bahnschrift", Font.BOLD, 16));
 		GridBagConstraints gbc_profile = new GridBagConstraints();
 		gbc_profile.insets = new Insets(0, 0, 0, 5);
 		gbc_profile.anchor = GridBagConstraints.WEST;
-		gbc_profile.gridx = 3;
+		gbc_profile.gridx = 4;
 		gbc_profile.gridy = 1;
 		panelNorte.add(profile, gbc_profile);
-		
+
 		JPanel panelCentral = new JPanel();
 		frame.getContentPane().add(panelCentral, BorderLayout.CENTER);
 		panelCentral.setLayout(new CardLayout(0, 0));
