8afd9e0763d82c663d201ee9c037fc8d4393fb55
Javi <jviix8@gmail.com>
2023-04-16 01:36:01 +0200

    Implementado descuento de likes + función top 10 likes de premium

--- PhotoTDS/src/umu/tds/model/LikesDiscount.java
+++ PhotoTDS/src/umu/tds/model/LikesDiscount.java
@@ -1,17 +1,20 @@
 package umu.tds.model;
 
+import java.util.List;
+
+import umu.tds.view.Constantes;
+
 public class LikesDiscount extends Discount {
 
 	@Override
 	public boolean validDiscountForUser(User user) {
-		// TODO Auto-generated method stub
-		return false;
+		List<Publication> allPubs = user.getPublications();
+		return allPubs.stream().mapToInt(Publication::getLikes).sum() > Constantes.MIN_LIKES_PREMIUM_DISCOUNT;
 	}
 
 	@Override
 	public int getPremiumDiscount(int price, User user) {
-		// TODO Auto-generated method stub
-		return 0;
+		return Constantes.LIKES_DISCOUNT;
 	}
 
 }
