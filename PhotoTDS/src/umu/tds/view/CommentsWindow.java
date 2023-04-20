96531d7d8ec35bed4aa214ea96cafe6049fd30dd
Ignacio <ignacio2002ruiz@gmail.com>
2023-04-20 15:55:52 +0200

    Boton Enviar/Borrar arreglado

--- PhotoTDS/src/umu/tds/view/CommentsWindow.java
+++ PhotoTDS/src/umu/tds/view/CommentsWindow.java
@@ -112,6 +112,7 @@ public class CommentsWindow {
 		btnEnviar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				Boolean enviar = false;
 				if (btnEnviar.getText().equals("Enviar")) {
 					Controller.getInstancia().addComment(pub, textoComentario.getText(), user.getUsername());
 					DefaultListModel<Comment> comments = Controller.getInstancia().getComments(pub.getTitle());
@@ -124,7 +125,7 @@ public class CommentsWindow {
 						Controller.getInstancia().removeComment(pub, removeComment);
 						removeComment = null;
 						publicationW.updateCommentsNumber();
-						btnEnviar.setText("Enviar");
+						enviar = true;
 					} else {
 						JOptionPane.showMessageDialog(frame,
 								"No tienes permiso para borrar el comentario " + "seleccionado", null,
@@ -137,6 +138,10 @@ public class CommentsWindow {
 						frame.getContentPane().revalidate();
 						frame.getContentPane().repaint();
 					}
+					
+					if (enviar) {
+						btnEnviar.setText("Enviar");
+					}
 
 				}
 			}
@@ -144,6 +149,7 @@ public class CommentsWindow {
 
 		commentsList.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent e) {
+				
 				btnEnviar.setText("Borrar");
 				if (!e.getValueIsAdjusting()) {
 					removeComment = commentsList.getSelectedValue();
